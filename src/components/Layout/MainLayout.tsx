67f7978228ca9d4b771d20272310400ee53dca99
andrei <andrei29.cervantes@gmail.com>
2026-03-10 01:28:27 +0800

    feat: add navbar

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,5 +1,12 @@
+import Navbar from "../Navbar/Navbar";
+
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="min-h-screen flex flex-col p-4">{children}</div>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      {children}
+    </div>
+  );
 };
 
 export default MainLayout;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="h-10 border-b flex justify-between px-4 items-center">
+      <p>B&W Shop</p>
+    </nav>
+  );
+};
+
+export default Navbar;
